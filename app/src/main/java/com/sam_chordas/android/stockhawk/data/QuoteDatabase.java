4bedbf933167c69f8ec6d92499cf939036d4655a
Tyler <caudell.tyler@gmail.com>
2016-10-10 12:55:44 -0500

    Reformatted the code and added some comments for potential new features.

--- app/src/main/java/com/sam_chordas/android/stockhawk/data/QuoteDatabase.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/data/QuoteDatabase.java
@@ -8,9 +8,11 @@ import net.simonvt.schematic.annotation.Table;
  */
 @Database(version = QuoteDatabase.VERSION)
 public class QuoteDatabase {
-  private QuoteDatabase(){}
 
-  public static final int VERSION = 7;
+    private QuoteDatabase(){}
 
-  @Table(QuoteColumns.class) public static final String QUOTES = "quotes";
+    public static final int VERSION = 7;
+
+    @Table(QuoteColumns.class)
+    public static final String QUOTES = "quotes";
 }
