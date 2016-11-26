8a74788d16d332ff7c79f5611b97ccad296ff605
Tyler Caudell <caudell.tyler@gmail.com>
2016-11-25 22:05:29 -0600

    Moved string constant to StockIntentService
    Moved the Intent String constant to StockIntentService, and updated the appropriate uses.

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StocksDetailActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StocksDetailActivity.java
@@ -8,15 +8,13 @@ import android.app.Activity;
 
 public class StocksDetailActivity extends Activity {
   
-  public static final String INTENT_SYMBOL = "intent_symbol";
-  
   @Override
   protected void onCreate(Bundle onSavedInstanceState) {
     super.onCreate(onSavedInstanceState);
     setContentView(R.layout.activity_temp_details);
     
     Intent intent = getIntent();
-    String symbol = intent.getStringExtra(INTENT_SYMBOL);
+    String symbol = intent.getStringExtra(StockIntentService.INTENT_SYMBOL);
     
     TextView symbolDisplay = (TextView) findViewById(R.id.details_debug_textview);
     symbolDisplay.append(symbol);
